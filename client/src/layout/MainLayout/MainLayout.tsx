dac390fa1a964a30c0ad1b249223ff77e5f9ba31
ysrckr <dyt.yasarcakir@gmail.com>
2023-02-22 14:22:18 +0300

    add basic styles

--- client/src/layout/MainLayout/MainLayout.tsx
+++ client/src/layout/MainLayout/MainLayout.tsx
@@ -2,8 +2,8 @@ import { Outlet } from 'react-router-dom';
 
 export const MainLayout = () => {
   return (
-    <>
+    <main>
       <Outlet />
-    </>
+    </main>
   );
 };
--- client/src/pages/Home/HomePage.tsx
+++ client/src/pages/Home/HomePage.tsx
@@ -1,5 +1,10 @@
 export const HomePage = () => {
   return (
-    <div>Home</div>
+    <div>
+      <h1>Home Page</h1>
+      <h2>Test</h2>
+      <h3>test</h3>
+      <h4>test</h4>
+    </div>
   )
 }
