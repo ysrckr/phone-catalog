e6b3256cb202971993f949eff6bd45e00ee93c19
ysrckr <dyt.yasarcakir@gmail.com>
2023-02-23 21:34:02 +0300

    add category services

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,11 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8080;
 
 
 const app = express();
@@ -12,6 +17,6 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.listen(PORT, () => {
+  console.log('Server started on http://localhost:' + PORT);
 });
---
+++ server/src/services/categories.ts
@@ -0,0 +1,128 @@
+import z from 'zod';
+import { prisma } from './dbConnection';
+
+export const getAll = async () => {
+  try {
+    const categories = await prisma.category.findMany();
+    return categories;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+};
+
+export const getOne = async (id: string) => {
+  const isValidId = z.string().uuid().safeParse(id).success;
+
+  if (!isValidId) {
+    return { error: 'Invalid id' };
+  }
+
+  try {
+    const category = await prisma.category.findUnique({
+      where: {
+        id,
+      },
+    });
+    return category;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+};
+
+export const create = async ({
+  name,
+  image,
+}: {
+  name: string;
+  image: string;
+}) => {
+  const isValidName = z.string().min(1).safeParse(name).success;
+
+  if (!isValidName) {
+    return { error: 'Invalid name' };
+  }
+
+  try {
+    const category = await prisma.category.create({
+      data: {
+        name,
+        image: image || '',
+      },
+    });
+    return category;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+};
+
+export const update = async ({
+  id,
+  name,
+  image,
+}: {
+  id?: string;
+  name: string;
+  image?: string;
+}) => {
+  if (id) {
+    const isValidId = z.string().uuid().safeParse(id).success;
+    const isValidName = z.string().min(1).safeParse(name).success;
+
+    if (!isValidId || !isValidName) {
+      return { error: 'Invalid id or name' };
+    }
+
+    try {
+      const category = await prisma.category.update({
+        where: {
+          id,
+        },
+        data: {
+          name,
+          image: image || '',
+        },
+      });
+      return category;
+    } catch (error) {
+      console.error(error);
+      return error;
+    }
+  }
+
+  try {
+    const category = await prisma.category.updateMany({
+      where: {
+        name,
+      },
+      data: {
+        image: image || '',
+      },
+    });
+    return category;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+};
+
+export const remove = async (id: string) => {
+  const isValidId = z.string().uuid().safeParse(id).success;
+
+  if (!isValidId) {
+    return { error: 'Invalid id' };
+  }
+
+  try {
+    const category = await prisma.category.delete({
+      where: {
+        id,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+ };
