a6eac414712f31ad9f38e87eb14d830cbfe73c40
ysrckr <dyt.yasarcakir@gmail.com>
2023-02-25 20:11:52 +0300

    problem with recieving cookies

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,3 +1,15 @@
+<script lang="ts" setup>
+import { auth } from './utils/authInterceptor';
+import { UserRegisterInfo } from './utils/authInterceptor';
+const user: UserRegisterInfo = {
+  name: 'test',
+  email: 'test@tt.com',
+  password: 'test',
+  role: 'ADMIN',
+}
+console.log(auth(user));
+</script>
+
 <template>
   <router-view></router-view>
 </template>
