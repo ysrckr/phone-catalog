a9287af12de7f4dcfe62bb2887c6349a69ed9722
ysrckr <dyt.yasarcakir@gmail.com>
2023-03-04 01:01:12 +0300

    create custom button with icon

---
+++ admin/src/components/ButtonWithIcon.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+const props = defineProps({
+  icon: {
+    type: String,
+    required: true,
+  },
+  iconSide: {
+    type: String,
+    default: 'left',
+  },
+  iconSize: {
+    type: Number,
+    default: 1.5,
+  },
+});
+</script>
+
+<template>
+  <button
+    class="text-center"
+    v-bind="$attrs"
+  >
+    <v-icon
+      v-if="iconSide === 'left'"
+      :icon="icon"
+      :size="`${iconSize}rem`"
+    />
+    <slot />
+    <v-icon
+      v-if="iconSide === 'right'"
+      :icon="icon"
+      :size="`${iconSize}rem`"
+    />
+  </button>
+</template>
--- admin/src/components/CategoriesItem.vue
+++ admin/src/components/CategoriesItem.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import ButtonWithIcon from './ButtonWithIcon.vue';
+
 const props = defineProps({
   name: {
     type: String,
@@ -13,35 +15,19 @@ const props = defineProps({
 
 <template>
   <div
-    class="drop-shadow gap-y-4 container flex flex-col w-1/2 p-4 bg-green-200 rounded-md"
+    class="drop-shadow gap-y-4 container flex flex-col w-1/3 p-4 bg-green-200 rounded-md"
   >
     <h4 class="font-semibold text-center capitalize">{{ props.name }}</h4>
     <img
       :src="props.image || 'https://fakeimg.pl/200x300/#f1f6fa/000'"
       class="w-200 h-300 mx-auto"
     />
-    <div class="flex justify-between items-center">
+    <div class="flex items-center justify-evenly">
       <div>
-        <button
-          class="hover:bg-yellow-300 inline-flex items-center gap-2 py-2 pl-2 pr-4 text-gray-600 bg-yellow-400 rounded-md"
-        >
-          <v-icon
-            icon="mdi-pencil"
-            size="1.5rem"
-          />
-          Edit
-        </button>
+        <ButtonWithIcon icon="mdi-pencil" class="hover:bg-yellow-300 inline-flex items-center gap-2 py-2 pl-2 pr-4 text-gray-600 bg-yellow-400 rounded-md"> Edit </ButtonWithIcon>
       </div>
       <div>
-        <button
-          class="hover:bg-red-400 inline-flex items-center gap-2 py-2 pl-2 pr-4 text-gray-200 bg-red-500 rounded-md"
-        >
-          <v-icon
-            icon="mdi-delete"
-            size="1.5rem"
-          />
-          Delete
-        </button>
+        <ButtonWithIcon icon="mdi-delete" class="hover:bg-red-300 inline-flex items-center gap-2 py-2 pl-2 pr-4 text-gray-200 bg-red-400 rounded-md"> Delete </ButtonWithIcon>
       </div>
     </div>
   </div>
