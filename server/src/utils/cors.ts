70127dca69cd2a8f5e48a5699fcbdbb807b1297c
ysrckr <dyt.yasarcakir@gmail.com>
2023-03-09 00:53:23 +0300

    make clientcors to give access to everyone

--- server/src/utils/cors.ts
+++ server/src/utils/cors.ts
@@ -13,19 +13,23 @@ export const adminCors = cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
 });
 
-const WhiteList = [
-  process.env.ADMIN_URL_1,
-  process.env.ADMIN_URL_2,
-  process.env.CLIENT_URL_1,
-  process.env.CLIENT_URL_2,
-];
-export const clientCors = cors({
-  origin: (origin, callback) => {
-    if (WhiteList.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET'],
-});
+//* Only allows the client to make GET requests
+// const WhiteList = [
+//   process.env.ADMIN_URL_1,
+//   process.env.ADMIN_URL_2,
+//   process.env.CLIENT_URL_1,
+//   process.env.CLIENT_URL_2,
+// ];
+// export const clientCors = cors({
+//   origin: (origin, callback) => {
+//     if (WhiteList.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   methods: ['GET'],
+// });
+
+// Allow all origins and methods
+export const clientCors = cors();
