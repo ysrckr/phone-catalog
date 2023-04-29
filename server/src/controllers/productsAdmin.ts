5657c7aa03329b683d99fe1883253515475d96de
Yasar Cakir <dev.yasarcakir@gmail.com>
2023-04-29 13:48:19 +0300

    create update function for productAdmin controller

--- server/src/controllers/productsAdmin.ts
+++ server/src/controllers/productsAdmin.ts
@@ -2,6 +2,7 @@ import type { Request, Response } from 'express';
 import {
   create as createProduct,
   remove as removeProduct,
+  update as updateProduct,
 } from '../services/products';
 import {
   cloudinaryDefaultUploadOptions,
@@ -74,6 +75,35 @@ export const create = async (req: Request, res: Response) => {
       categoryId: category,
     });
     return res.status(201).json(product);
+  } catch (error) {
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+export const update = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  if (!id) {
+    return res.status(400).json({ error: 'Missing id' });
+  }
+
+  const { name, description, price, quantity, colors, sizes, category } =
+    req.body;
+  const images = req.files as Express.Multer.File[];
+
+  try {
+    const product = await updateProduct({
+      id,
+      name,
+      description,
+      price: Number(price),
+      quantity: Number(quantity),
+      colors,
+      sizes,
+      categoryId: category,
+      images: images.map(image => image.path),
+    });
+    return res.status(200).json(product);
   } catch (error) {
     return res.status(500).json({ error: 'Internal server error' });
   }
