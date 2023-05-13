ad9590f163d9e8251bea1e811a283823f58f2bb2
Yasar Cakir <dev.yasarcakir@gmail.com>
2023-05-13 15:16:56 +0300

    Create ErrorResponse class and complete error middleware

--- server/src/middleware/error.ts
+++ server/src/middleware/error.ts
@@ -1,8 +1,17 @@
 import type { NextFunction, Request, Response } from 'express';
+import type { ErrorResponse } from '../utils/ErrorResponse';
 
 export const errorHandler = (
-  err: Error,
+  err: ErrorResponse,
   req: Request,
   res: Response,
   next: NextFunction,
-) => {};
+) => {
+  if (process.env.NODE_ENV === 'development') {
+    console.log(err.stack);
+  }
+
+  res.status(err.statusCode || 500).json({
+    error: err.message || 'Server Error',
+  });
+};
